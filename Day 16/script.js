0d670382380e328b76cb76d7642a0fcd27d9ce06
maczko22 <32737865+maczko22@users.noreply.github.com>
2022-02-01 12:00:38 +0100

    Add files via upload

---
+++ Day 16/script.js
@@ -0,0 +1,48 @@
+const smallCups = document.querySelectorAll('.cup-small')
+const listers = document.getElementById('liters')
+const percentage = document.getElementById('percentage')
+const remained = document.getElementById('remained')
+
+updateBigCup()
+
+smallCups.forEach((cup, idx) => {
+    cup.addEventListener('click', () => highlightCups(idx))
+})
+
+function highlightCups(idx) {
+    if (smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+        idx--
+    }
+    
+    smallCups.forEach((cup, idx2) => {
+        if(idx2 <= idx) {
+            cup.classList.add('full')
+        } else {
+            cup.classList.remove('full')
+        }
+    })
+
+    updateBigCup()
+}
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const totalCaps = smallCups.length
+
+    if(fullCups === 0) {
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    }else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCaps * 330}px`
+        percentage.innerHTML = `${fullCups / totalCaps * 100}`
+    }
+
+    if(fullCups === totalCaps) {
+        remained.style.visibility = 'hidden'
+        remained.style.height = 0
+    } else {
+        remained.style.visibility = 'visible'
+        listers.innerHTML = `${2 - (250 * fullCups / 1000)}L`
+    }
+}
